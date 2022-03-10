74fa82c795d36eb76993fed4e66e6fe9dc836f3f
박수현 <57443458+hyun5292@users.noreply.github.com>
2022-03-11 02:04:07 +0900

    제 20장 미니 프로젝트 Todo App 1

---
+++ BASIC_VUE/todo/src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="app" class="container">
+    <h1 class="text-center">경원이의 하루</h1>
+    <!-- 이 정도면 v-model은 만능인 것인가…? -->
+    <input 
+      v-model="todoText"
+      class="w-100 p-2" 
+      type="text" 
+      placeholder="Type todo"
+      @keyup.enter="addTodo"
+    >
+    <hr>
+    <TodoItem 
+      v-for="todo in todos" 
+      :key="todo.id"
+      :todo="todo"
+    />
+    <!-- :todo는 props -->
+  </div>
+</template>
+
+<script>
+import TodoItem from '@/components/TodoItem.vue';
+
+export default {
+  components: {
+    TodoItem
+  },
+  data() {
+    return {
+      todoText: '',
+      todos: [
+        { id: 1, text: '9시까지 출근하기', checked: false },
+        { id: 2, text: '시스템 조사하기(?)', checked: false },
+      ]
+    }
+  },
+  methods: {
+    addTodo(e) {
+      this.todos.push({
+        id: Math.random(),  //중복될 수 있지만 임시로 한 것
+        text: e.target.value,
+        checked:false
+      });
+      //input 박스에 내용 입력하고 enter 누르면 빈칸이 되도록 하는 것
+      this.todoText = '';
+    }
+  }
+}
+</script>
+
+<style>
+/* 이건 글씨가 작아서 내가 임의로 추가한 것 */
+body { font-size: 1.5rem !important; }
+</style>
---
+++ BASIC_VUE/todo/src/components/TodoItem.vue
@@ -0,0 +1,22 @@
+<template>
+    <!-- 제 16장 자식 컴포넌트에서 데이터 보내기(Props) -->
+    <div>
+        <input type="checkbox" :checked="todo.checked">
+        <span class="m-3">{{ todo.text }}</span>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        todo: {
+            type: Object,
+            required: true
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
