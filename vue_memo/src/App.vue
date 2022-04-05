d088fb21048f6f0f5cd0530838edc5aeaaf0d563
박수현 <57443458+hyun5292@users.noreply.github.com>
2022-04-05 17:45:20 +0900

    Vue3 강의 - REST API를 이용하여 메모 애플리케이션 만들기
    프로젝트 환경 설정 위주
    1 ~ 7번까지

---
+++ vue_memo/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="app">
+    <Ground />
+  </div>
+</template>
+
+<script>
+import Ground from "./components/comGround.vue";
+
+export default {
+  components: {
+    Ground
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue_memo/src/components/comGround.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="ground">
+    <Header />
+    <Memo />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Header from './comHeader.vue';
+import Memo from './comMemo.vue';
+import Footer from './comFooter.vue';
+
+export default {
+  components: {
+    Header,
+    Memo,
+    Footer,
+  }
+}
+</script>
+
+<style>
+
+</style>
