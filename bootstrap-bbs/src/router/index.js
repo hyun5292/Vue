556cfd344b46e323c54302e161de0a9e22f8dc77
박수현 <57443458+hyun5292@users.noreply.github.com>
2022-04-20 20:16:43 +0900

    Bootstrap-Vue로 게시판 만들기
    Vue.js로 게시판 만들기 마무리
    &
    1 ~ 7번까지

---
+++ bootstrap-bbs/src/router/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import Board from '@/components/boardCom.vue'
+
+Vue.use(VueRouter)
+
+export default new VueRouter({
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/board/free',
+      name: 'Board',
+      component: Board
+    },
+  ]
+});
