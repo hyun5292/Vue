c1006199bb2c76c329675ff63cc745f4ee555457
박수현 <57443458+hyun5292@users.noreply.github.com>
2022-04-25 19:48:51 +0900

    Bootstrap-Vue로 게시판 만들기
    12 ~ 13 번까지
    덧글 구현 & 작성

---
+++ bootstrap-bbs/src/components/cmtCreateCom.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+      <b-input-group :prepend="name" class="mt-3">
+          <b-form-textarea
+            id="textarea"
+            v-model="context"
+            :placeholder="'코멘트를 달아주세요~!'"
+            rows="3"
+            max-rows="6"
+          ></b-form-textarea>
+      </b-input-group>
+      <b-input-group-append>
+          <b-button variant="info" @click="createComment">작성하기</b-button>
+      </b-input-group-append>
+  </div>
+</template>
+
+<script>
+import data from '@/data';
+
+export default {
+    name: "cmtCreateCom",
+    props: {
+        contentId: Number,
+        reloadComment: Function,
+    },
+    data() {
+        return {
+            name: "르라나",
+            context: ""
+        }
+    },
+    methods: {
+        createComment() {
+            data.Comment.push(
+                {
+                    comment_id: data.Comment[data.Comment.length - 1].comment_id + 1,
+                    user_id: 1,
+                    content_id: this.contentId,
+                    context: this.context,
+                    created_at: '2022-04-25 14:11:11',
+                    updated_at: null
+                }
+            );
+            this.reloadComment();
+            this.context = "";
+        },
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ bootstrap-bbs/src/components/cmtListCom.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+      <div :key="item.comment_id" v-for="item in comments">
+          <CmtListItem :commentObj="item" />
+      </div>
+      <CmtCreate :contentId="contentId" :reloadComment="reloadComment"/>
+  </div>
+</template>
+
+<script>
+import data from '@/data';
+import CmtListItem from './cmtListItemCom.vue';
+import CmtCreate from './cmtCreateCom.vue';
+
+export default {
+    name: 'cmtListCom',
+    props: {
+        contentId: Number,
+    },
+    components: {
+        CmtListItem,
+        CmtCreate,
+    },
+    data() {
+        return {
+            comments: data.Comment.filter(item => item.content_id === this.contentId)
+        }
+    },
+    methods: {
+        reloadComment() {
+            this.comments = data.Comment.filter(item => item.content_id === this.contentId)
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
