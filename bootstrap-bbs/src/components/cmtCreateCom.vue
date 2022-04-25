b08a7e498f18dc84bee2a25cbb4ae1876f95c24b
박수현 <57443458+hyun5292@users.noreply.github.com>
2022-04-26 02:12:40 +0900

    Bootstrap-Vue로 게시판 만들기
    14 번까지
    대댓글

--- bootstrap-bbs/src/components/cmtCreateCom.vue
+++ bootstrap-bbs/src/components/cmtCreateCom.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="comment-create">
       <b-input-group :prepend="name" class="mt-3">
           <b-form-textarea
             id="textarea"
@@ -10,7 +10,7 @@
           ></b-form-textarea>
       </b-input-group>
       <b-input-group-append>
-          <b-button variant="info" @click="createComment">작성하기</b-button>
+          <b-button variant="info" @click="isSubComment ? createSubComment() : createComment()">작성하기</b-button>
       </b-input-group-append>
   </div>
 </template>
@@ -22,7 +22,11 @@ export default {
     name: "cmtCreateCom",
     props: {
         contentId: Number,
+        commentId: Number,
+        isSubComment: Boolean,
         reloadComment: Function,
+        subCommentToggle: Function,
+        reloadSubComments: Function,
     },
     data() {
         return {
@@ -45,10 +49,28 @@ export default {
             this.reloadComment();
             this.context = "";
         },
+        createSubComment() {
+            data.SubComment.push(
+                {
+                    subcomment_id: data.SubComment[data.SubComment.length - 1].subcomment_id + 1,
+                    user_id: 1,
+                    comment_id: this.commentId,
+                    context: this.context,
+                    created_at: '2022-04-25 14:11:11',
+                    updated_at: null
+                }
+            );
+            this.subCommentToggle();
+            this.reloadSubComments();
+            this.context = "";
+        },
     }
 }
 </script>
 
-<style>
-
+<style scoped>
+.comment-create {
+  display: flex;
+  margin-bottom: 1em;
+}
 </style>
--- bootstrap-bbs/src/components/cntDetailCom.vue
+++ bootstrap-bbs/src/components/cntDetailCom.vue
@@ -70,6 +70,42 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+.content-detail-content-info {
+  border: 1px solid black;
+  display: flex;
+  justify-content: space-between;
+}
+.content-detail-content-info-left {
+  width: 720px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem;
+}
+.content-detail-content-info-right {
+  width: 300px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 1rem;
+}
+.content-detail-content {
+  border: 1px solid black;
+  margin-top: 1rem;
+  padding-top: 1rem;
+  min-height: 720px;
+}
+.content-detail-button {
+  border: 1px solid black;
+  margin-top: 1rem;
+  padding: 2rem;
+}
+.content-detail-comment {
+  border: 1px solid black;
+  margin-top: 1rem;
+  padding: 2rem;
+}
 </style>
