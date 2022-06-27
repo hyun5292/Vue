064816fc43a939781045a6b8e443ce02e70904e4
박수현 <57443458+hyun5292@users.noreply.github.com>
2022-06-27 23:55:34 +0900

    글 작성 화면 디자인 중
    table border 겹치는 현상 수정 중
    border-collapse로 고치는 건 알겠음

--- factoreview/src/views/QnaWriteView.vue
+++ factoreview/src/views/QnaWriteView.vue
@@ -7,23 +7,23 @@
         </div>
         <table class="info">
           <tr>
-              <td colspan="2">
-                <input type="text" placeholder="제목을 입력해주세요">
+              <td colspan="2" class="info_input">
+                <input type="text" id="info_title" placeholder="제목을 입력해주세요">
               </td>
           </tr>
           <tr>
-              <td width="15%">아이디1</td>
-              <td width="75%">2020-12-25</td>
+              <td width="15%" class="info_coll"><span>아이디1</span></td>
+              <td width="85%" class="info_coll"><span>2020-12-25</span></td>
           </tr>
           <tr>
-              <td>첨부파일</td>
-              <td>
+              <td class="info_coll"><span>첨부파일</span></td>
+              <td class="info_coll">
                 <button>내PC</button>
               </td>
           </tr>
           <tr class="table_cont_tr">
-              <td colspan="2" class="table_cont_td">
-                  <textarea name="" id="" cols="30" rows="10"></textarea>
+              <td colspan="2" class="table_cont_td info_input">
+                  <textarea name="" id="info_cont" cols="30" rows="10"></textarea>
               </td>
           </tr>
       </table>
@@ -57,6 +57,10 @@ $black: #3b3d3c;
 $dark-blue: #023859;
 $light-gray2: #eaeaea;
 
+tr, td {
+  padding: 0px;
+}
+
 .qWrite {
   display: flex;
   .cont {
@@ -64,7 +68,6 @@ $light-gray2: #eaeaea;
     width: calc(100% - 250px);
     padding: 20px;
     #fact_name {
-        margin: 10px 0;
         padding: 5px 0;
         width: 100%;
         color: white;
@@ -74,12 +77,39 @@ $light-gray2: #eaeaea;
     }
     .info {
       margin: 10px 0;
+      width: 100%;
+      border-spacing: 0px;
+      //border-collapse: collapse;
+      background-color: white;
+      span {
+        padding: 5px;
+      }
+      #info_title {
+        box-sizing: border-box;
+        width: 100%;
+        height: 40px;
+        font-size: 1rem;
+        border-radius: 5px 5px 0 0;
+        border: 1px solid $black;
+      }
+      #info_cont {
+        box-sizing: border-box;
+        width: 100%;
+        height: 50vh;
+        font-size: 1.2rem;
+        border-radius: 0 0 5px 5px;
+      }
+      .info_input {
+        padding: none;
+        border: none;
+      }
     }
     .btns {
       display: flex;
       justify-content: space-between;
       text-align: center;
       .toqList, .write_btn {
+        padding: 5px 0;
         width: calc(50% - 5px);
         &:hover {
           background-color: $dark-blue;
